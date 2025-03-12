8254fc352be2308391d4131fa71657d4bc1b15fa
Avi <liamswift2014@gmail.com>
2025-03-12 00:17:44 -0400

    Add Sentry error monitoring and performance tracking

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
+// IMPORTANT: Make sure to import `instrument.js` at the top of your file.
+// If you're using ECMAScript Modules (ESM) syntax, use `import "./instrument.js";`
+import "./instrument.js";
+
 import express from "express";
 import http from "http";
+import * as Sentry from "@sentry/node";
 import { Server } from "socket.io";
 import cors from "cors";
 import bodyParser from "body-parser";
@@ -21,12 +26,23 @@ const PORT = process.env.PORT || 8001;
 app.use(cors());
 app.use(bodyParser.json());
 
+Sentry.setupExpressErrorHandler(app);
+
 app.get("/", (req, res) => {
     res.send("Server working");
 });
 
 app.use("/api", apiRouter);
 ConnectDatabase();
+
+
+// Optional fallthrough error handler
+app.use(function onError(err, req, res, next) {
+  // The error id is attached to `res.sentry` to be returned
+  // and optionally displayed to the user for support.
+  res.statusCode = 500;
+  res.end(res.sentry + "\n");
+});
 
 io.on("connection", (socket) => {
     socket.on("message", (data) => {
---
+++ instrument.js
@@ -0,0 +1,14 @@
+import * as Sentry from "@sentry/node";
+import { nodeProfilingIntegration } from "@sentry/profiling-node";
+
+Sentry.init({
+  dsn: "https://ad7c753df95f486c0775673f7664ac9d@sen.newhoopla.com/3",
+  integrations: [
+    nodeProfilingIntegration(),
+  ],
+  // Tracing
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+
+  // Set sampling rate for profiling - this is relative to tracesSampleRate
+  profilesSampleRate: 1.0,
+});

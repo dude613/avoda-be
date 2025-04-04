65faef5a4c9f1cdc0d59cd72cb86f173043225da
Avi <liamswift2014@gmail.com>
2025-04-04 00:02:56 -0400

    Refactor constants structure with categorized organization

--- Constants/UserConstants.js
+++ Constants/UserConstants.js
@@ -1,25 +1,33 @@
 
 export const userContent = {
-    EMAIL_NOT_FOUND_ERROR : "This email doesn't exist in our database. Please try another email.",
-    EMAIL_REQUIRED_ERROR : "email is required!",
-    INVALID_EMAIL_FORMAT_ERROR : "Invalid email format!",
-    PASSWORD_REQUIRED_ERROR : "Password is required!",
-    PASSWORD_REQUIRED_INCORRECT : "Your password is not correct!",
-    PASSWORD_COMPLEXITY_ERROR : "Password must be at least 8 characters long, include an uppercase letter, a number, and a special character.",
-    GENERIC_ERROR_MESSAGE : "An error occurred while processing your request.",
-    USER_EMAIL_ALREADY_EXIST : "This email is already registered. Please use a different email!",
-    USER_EMAIL_ALREADY_VERIFIED :  "This email already verified!",
-    USER_INVALID_OTP : "Invalid OTP please check and try again.",
-    USER_OTP_EXPIRE : "OTP expired. Please request a new one.",
-    USER_EMAIL_VERIFIED : "Email verified successfully!",
-    PASSWORD_RESET_EMAIL_SENT : "A password reset link has been sent to your email. Please check your inbox and follow the instructions to reset your password.",
-    PASSWORD_UPDATED_SUCCESS : "Password updated successfully!",
-    USER_LOGIN_SUCCESS : "User logged in successfully",
-    USER_REGISTER_SUCCESS : "User Registered successfully",
-    USER_SEND_OTP : "OTP has been sent to your registered email address!",
-    USER_PROFILE_DATA_SUCCESS : "user details fetch successfully",
-    EMAIL_REGEX : /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
-    PASSWORD_REGEX : /^(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
+    errors:{
+        EMAIL_NOT_FOUND_ERROR : "This email doesn't exist in our database. Please try another email.",
+        EMAIL_REQUIRED_ERROR : "email is required!",
+        INVALID_EMAIL_FORMAT_ERROR : "Invalid email format!",
+        PASSWORD_REQUIRED_INCORRECT : "Your password is not correct!",
+        GENERIC_ERROR_MESSAGE : "An error occurred while processing your request.",
+        USER_EMAIL_ALREADY_EXIST : "This email is already registered. Please use a different email!",
+        USER_EMAIL_ALREADY_VERIFIED :  "This email already verified!",
+        USER_INVALID_OTP : "Invalid OTP please check and try again.",
     OTP_NOT_SENT : "Failed to send OTP"
+    },
+    success:{
+        USER_EMAIL_VERIFIED : "Email verified successfully!",
+        PASSWORD_UPDATED_SUCCESS : "Password updated successfully!",
+        USER_LOGIN_SUCCESS : "User logged in successfully",
+        USER_REGISTER_SUCCESS : "User Registered successfully",
+        USER_PROFILE_DATA_SUCCESS : "user details fetch successfully",
+    },
+    messages: {
+        PASSWORD_REQUIRED_ERROR : "Password is required!",
+        PASSWORD_COMPLEXITY_ERROR : "Password must be at least 8 characters long, include an uppercase letter, a number, and a special character.",
+        USER_OTP_EXPIRE : "OTP expired. Please request a new one.",
+        PASSWORD_RESET_EMAIL_SENT : "A password reset link has been sent to your email. Please check your inbox and follow the instructions to reset your password.",
+        USER_SEND_OTP : "OTP has been sent to your registered email address!",
+    },
+    validations: {
+        EMAIL : /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+        PASSWORD : /^(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
+    }
 }
 
